7ba52f76203c5c022d1b0841e4baf4d0987ae351
Fernando <ferry.fernando@gmail.com>
2018-10-31 09:52:59 +0700

    Create the model.

--- server/movies/models.py
+++ server/movies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    """ Genre of the movie (Action, Thriller, etc.) """
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    """ The movie model """
+    name = models.CharField(max_length=250)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    stars = models.FloatField(default=1.0)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    """ Comment from users about the movie """
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE)
+    comment = models.TextField()
+    visible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
