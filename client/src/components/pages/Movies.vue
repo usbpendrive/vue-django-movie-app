638597201bc4a77a21d954d88c268ec17b91d2ee
Fernando <ferry.fernando@gmail.com>
2018-10-31 11:00:26 +0700

    Auth pages.

---
+++ client/src/components/pages/Movies.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      Movie page
+    </div>
+</template>
+
+<script>
+import router from '../../router';
+
+export default {
+  name: "Movies",
+  mounted() {
+    this.checkLoggedIn();
+  },
+  methods: {
+    checkLoggedIn() {
+      router.push('/auth');
+    }
+  }
+};
+</script>
